da6593b1db82afb79eadad1ee3416f4c0e38db3d
Delia Varzariu <dell.varzariu@gmail.com>
2022-12-09 10:25:57 +0200

    First commit

--- App.tsx
+++ App.tsx
@@ -1,10 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View } from "react-native";
+import * as Notifications from "expo-notifications";
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
 
 export default function App() {
+  async function scheduleNotification() {
+    await Notifications.scheduleNotificationAsync({
+      content: {
+        title: "My first notification",
+        body: "This is the notification body",
+        data: {
+          username: "Matt",
+        },
+      },
+      trigger: {
+        seconds: 10,
+      },
+    });
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Button title="Schedule Notification" onPress={scheduleNotification} />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +37,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
